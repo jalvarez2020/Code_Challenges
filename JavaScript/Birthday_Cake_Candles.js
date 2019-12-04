a057637e79c83c37c5d6bdf316a59de13fb5b1ae
Javier Alavrez <javieralvarez82@live.com>
2019-12-04 09:30:23 -0800

    complete in Birthday Cake All test cases pass

--- JavaScript/Birthday_Cake_Candles.js
+++ JavaScript/Birthday_Cake_Candles.js
@@ -1,24 +1,39 @@
 //https://www.hackerrank.com/challenges/birthday-cake-candles/submissions/code/132953698
 
-
+//Code passes all test cases.
 function birthdayCakeCandles(ar) {
     //find the tallest candles and count them
-    //sort the array from smallest to larggest
+    //sort the array from smallest to largest
     //check if the largest has duplicates, if it does count them
     let count = 0
     let currentArr = ar
-    currentArr.sort();
-    let larCandle = currentArr[ar.length - 1]
-    let largest = new Set([larCandle])
+    let larCandle = Math.max(...currentArr)
     for(let i = 0; i < currentArr.length; i++){
-        if( largest.has(currentArr[i]) ){
+        if( larCandle === currentArr[i] ){
             count += 1
         }
-     
     }
 
     return count
-    
-
-
 }
+
+
+//Inefficient Code time complexity = O(n)2
+// function birthdayCakeCandles(ar) {
+//     //find the tallest candles and count them
+//     //sort the array from smallest to largest
+//     //check if the largest has duplicates, if it does count them
+//     let count = 0
+//     let currentArr = ar
+//     currentArr.sort(); 
+//     let larCandle = currentArr[ar.length - 1]
+//     let largest = new Set([larCandle])
+//     for(let i = 0; i < currentArr.length; i++){
+//         if( largest.has(currentArr[i]) ){
+//             count += 1
+//         }
+     
+//     }
+//     return count
+    
+// }
