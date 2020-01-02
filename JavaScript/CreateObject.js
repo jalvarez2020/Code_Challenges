36b4cc0f987449cb2b345122b6716900c68e860a
Javier Alavrez <javieralvarez82@live.com>
2020-01-02 06:44:16 -0800

    Complete Create a Rectangle Object

---
+++ JavaScript/CreateObject.js
@@ -0,0 +1,9 @@
+//https://www.hackerrank.com/challenges/js10-objects/problem
+
+function Rectangle(a, b) {
+    this.length = a
+    this.width = b
+    this.perimeter = a + a + b + b
+    this.area = a * b
+
+}
