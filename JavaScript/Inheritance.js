3c108a82039aabfadb09fbbf5d8377f29b1cc61f
Javier Alavrez <javieralvarez82@live.com>
2020-01-02 07:55:46 -0800

    Complete Inheritance

---
+++ JavaScript/Inheritance.js
@@ -0,0 +1,28 @@
+//https://www.hackerrank.com/challenges/js10-inheritance/problem
+
+
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+
+Rectangle.prototype.area = function() {
+    return this.w * this.h 
+}
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle {
+    constructor(side){
+        super(side)
+        this.w = side
+        this.h = side
+    }
+}
